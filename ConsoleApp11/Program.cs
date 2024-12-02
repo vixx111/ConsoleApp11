19e26ee42581d770d107e19d4fa72d46d9954813
Admin <Admin@DESKTOP-UGP0C2G>
2024-12-02 13:40:48 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,28 @@
+﻿        Console.Write("Введите значение x: ");
+        double x = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введите количество членов n: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        if (n < 1)
+        {
+            Console.WriteLine("Количество членов n должно быть положительным числом!");
+            return;
+        }
+        double sum = 1.0;
+        for (int i = 1; i < n; i++)
+        {
+            double term = Math.Pow(x, 2 * i) / Factorial(2 * i);
+            sum += term;
+        }
+        Console.WriteLine($"Сумма первых {n} членов ряда = {sum}");    
+    static double Factorial(int num)
+    {
+        double result = 1;
+        for (int i = 1; i <= num; i++)
+        {
+            result *= i;
+        }
+        return result;
+    }
+
